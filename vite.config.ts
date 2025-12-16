6e85d54c7ffbe058d2c487dbdaf48827a2dcd62f
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2025-12-16 21:58:00 +0800

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -9,10 +10,15 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
+      plugins: [
+        react(),
+        VitePWA({
+          // Optional: add your configuration options here
+          registerType: 'autoUpdate', // Automatically update when new content is available
+          // ... more options like manifest, workbox, etc.
+        })
+      ],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
